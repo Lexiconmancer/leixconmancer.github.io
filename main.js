5e702d65c0c5dccd853a945f3b5be1a2886c21f0
Lexiconmancer <111313430+Lexiconmancer@users.noreply.github.com>
2022-08-15 16:38:21 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,83 @@
+
+/* TODO
+make sniff its own separate clickable
+boops buy things that increment sniffs
+sniffs add more things to explore in the world
+resource: grass that can be accessed spending sniffs (occasionally find dirt in grass, more rarely other things)
+Achievements
+    Bad dog - unlock 'learning'
+    Make a friend (vs. sources: petstore, next door lawn) First time unlocks additional puppies, others add to count
+        
+action: mark territory (this should be a one-time thing for specific landmarks like fire hydrants, home, next door lawn, petstore, home, show floor, etc)
+create home system (this should provide various upgrades to stats and be upgradeable by resource:behavior)
+resource: behavior (related achievements: bad dog!, good boy!, etc)
+resource: learning (good boy!, etc)
+    training (once other puppies)
+
+*/
+
+/*
+Load file when page opens
+*/
+window.onload = load
+
+/*
+    boops and sniffs
+*/
+
+var boops = 0;
+function boopsClick(bn){
+    boops = boops + bn;
+    document.getElementById("boops").innerHTML = prettify(boops);
+};
+
+var sniffs = 0;
+function buySniffs(){
+    var sniffsCost = Math.floor(10 * Math.pow(1.1,sniffs));             //determines cost of sniffs
+    if(boops >= sniffsCost){                                            //check user can afford sniffs
+        sniffs = sniffs + 1;                                            //increases sniffs when bought
+        boops = boops - sniffsCost;                                     //removes spent boops
+        document.getElementById('sniffs').innerHTML = sniffs;           //updates displayed sniffs
+        document.getElementById('boops').innerHTML = boops;             //updates displayed boops
+    };
+    var nextSniffsCost = Math.floor(10 * Math.pow(1.1,sniffs));         //determines increased cost of next boop
+    document.getElementById('sniffsCost').innerHTML = nextSniffsCost    //updates displayed boop cost
+};
+
+
+/*
+Main game loop
+*/
+window.setInterval(function(){
+
+    boopsClick(sniffs/10);
+
+}, 1000);
+
+/*
+Decimal display
+*/
+function prettify(input){
+    var output = Math.round(input * 1000000)/1000000;
+        return output;
+}
+
+/*
+localStorage Saving
+*/
+
+var save = {
+    boops: boops,
+    sniffs: sniffs,
+    prestige: prestige
+}
+localStorage.setItem("save",JSON.stringify(save));
+
+/*
+localStorage Loading
+*/
+function load(){
+var savegame = JSON.parse(localStorage.getItem("save"));
+if (typeof savegame.boops !== "undefined") boops = savegame.boops;
+if (typeof savegame.sniffs !== "undefined") sniffs = savegame.sniffs;
+}
